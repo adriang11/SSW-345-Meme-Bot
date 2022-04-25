4fee13ebc0a907d1dbcca74df9464e7a6290efc9
Adrian <adgarcia1119@gmail.com>
2022-04-24 22:13:55 -0400

    finished meme

--- jimp.js
+++ jimp.js
@@ -1,9 +1,9 @@
 const Jimp = require('jimp');
 
 async function textOverlay() {
-   const image = await Jimp.read('./meme_formats/goosebumps.jpg');
-   const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
-   image.print(font, 120, 110, 'test text');
+   const image = await Jimp.read('./meme_formats/yoda.jpg');
+   const font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
+   image.print(font, 150, 500, 'test text');
    await image.writeAsync('./output.png');
 }
 
